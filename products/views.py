d91ac762e14d7eb88797c457b09d08d42b9abdef
Tsz Fung Ho <hotszfung0910@gmail.com>
2022-01-29 22:40:41 +0000

    Initial commit

---
+++ products/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# /products -> index
+# Uniform Resource Locator (Address)
+
+
+def index(request):
+    return HttpResponse('Hello World')
+
+
+def new(request):
+    return HttpResponse('New Products')
+
+
